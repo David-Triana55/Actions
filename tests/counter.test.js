14a4d7c741732161a18aa68a5fa6b88bcf406495
David Triana <davidtriana796@gmail.com>
2024-09-24 21:16:45 -0500

    fix path to file

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -1,4 +1,4 @@
-import { setupCounter } from './counter.js';
+import { setupCounter } from '../counter.js';
 import { beforeEach, describe, expect, it } from 'vitest';
 
 describe('Counter Button', () => {
